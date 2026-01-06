8830b051e5865d098d9e9e1da8725d75bc8831eb
Eniolasoar <eniolanishe@gmail.com>
2026-01-06 19:41:14 +0100

    Go back bug fix in blog page

--- blog/getpost.js
+++ blog/getpost.js
@@ -64,7 +64,10 @@ function renderPost(id, p) {
   backBtn.textContent = "← Go Back";
   backBtn.style =
     "margin-bottom:20px;padding:6px 12px;border:none;background:#7528FF;color:#fff;border-radius:4px;cursor:pointer;";
-  backBtn.onclick = () => window.history.back();
+    backBtn.onclick = () => {
+      window.location.href = "blog.html";
+    };
+    
   postContainer.appendChild(backBtn);
 
   // Title
